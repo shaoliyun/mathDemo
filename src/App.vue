c8afeb4158fd4a86908210d2f6097f7817504ada
shaoliyun <shaoliyun20021125@163.com>
2024-07-25 20:39:28 +0800

    mathDemo

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <katex-element expression="\\ \mathcal{P} = (t,\mathbf{G}')"/> -->
+    <MathType></MathType>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import MathType from './components/mathType.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MathType
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+@import "../node_modules/katex/dist/katex.min.css";
+
+.katex-html {
+  display: none;
+}
+
+</style>
---
+++ src/components/mathType.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <katex-element expression="\\ \mathcal{P} = (t,\mathbf{G}')"/>
+  </div>
+    
+</template>
+
+<script>
+
+export default{
+  name:'mathType',
+}
+</script>
+
+<style>
+</style>
